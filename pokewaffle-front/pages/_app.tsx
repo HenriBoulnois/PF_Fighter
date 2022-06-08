2514425ebf53451dbce11b45315d11dbc60af241
HenriBoulnois <boulnois.henri@gmail.com>
2022-06-08 23:11:15 +0200

    :lipstick: Site title

--- pokewaffle-front/pages/_app.tsx
+++ pokewaffle-front/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '../styles/tailwind.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <div>
+    <Head>
+    <title>Poke fighter</title>
+    <link rel="icon" href="dedenne.png" />
+    </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      </div>
   )
 }
 
